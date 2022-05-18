8f2f850dc7df7fc721483287c948c134c8d2f4a4
Daneric <danericaalbers@hotmail.com>
2022-05-18 12:09:18 +0200

    feat: get data from api

---
+++ src/components/ApiData.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Data</h1>
+    <p>{{ list }}</p>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ApiData',
+  data() {
+    return {
+      list: [],
+    };
+  },
+  async mounted() {
+    const result = await axios.get('http://localhost:3000/api');
+    console.warn(result);
+    // convert the result to json
+    this.list = result.data.data;
+  },
+};
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,14 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <ApiData />
 </template>
 
 <script>
+import ApiData from '@/components/ApiData.vue';
 
 export default {
   name: 'AboutView',
   mounted() {
     document.title = 'RemVee - About';
+  },
+  components: {
+    ApiData,
   },
 };
 </script>
