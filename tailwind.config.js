0f4241cb760e10f89716c333bde13397323dc4fc
Daneric <danericaalbers@hotmail.com>
2022-05-19 11:00:49 +0200

    feat: own color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      'primary': '#41B883',
+      'secondary': '#34495E',
+  },
   },
   plugins: [],
 };
