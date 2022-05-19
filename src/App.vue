5135af3fc7d58bc9e2b96fa3e3eb3c034ce966f3
Daneric <danericaalbers@hotmail.com>
2022-05-19 10:11:47 +0200

    feat: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
+<div>
 <link rel="stylesheet" href="/css/">
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<!-- eslint-disable max-len -->
+  <div class="flex flex-row items-center p-2 bg-emerald-600">
+    <div class="flex w-1/4">
+        <h1 class="text-xl font-semibold">Remvee</h1>
+    </div>
+    <div class="flex flex-row justify-end float-right w-3/4 text-right text-gray-700">
+        <router-link class="w-1/12 mx-2 font-semibold text-center bg-white border-2 border-gray-700 rounded-md" to="/">Home</router-link>
+        <router-link class="w-1/12 mx-2 font-semibold text-center bg-white border-2 border-gray-700 rounded-md" to="/about">About</router-link>
+    </div>
+  </div>
   <router-view/>
+<!-- eslint-enable max-len -->
+</div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
