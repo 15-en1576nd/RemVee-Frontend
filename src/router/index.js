dbe238c2469f2564e846fbceb27d9a43c262c317
Daneric <danericaalbers@hotmail.com>
2022-05-19 11:32:03 +0200

    feat: dashboard route

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/DashboardView.vue'),
+  },
 ];
 
 const router = createRouter({
