0152a3205de2c622275fd45b149ce8f56722d795
Chris Friemann <github@chris.friemann.nl>
2022-06-14 10:26:22 +0200

    Feat: added overview dashboard route

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/DashboardView.vue'),
   },
+  {
+    path: '/admin/dashboard',
+    name: 'Admin Dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminDashboardView.vue'),
+  },
 ];
 
 const router = createRouter({
