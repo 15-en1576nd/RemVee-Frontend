a33dc95d5befbe86fc9f238e92b81998c057ce3a
Chris Friemann <github@chris.friemann.nl>
2022-06-21 09:52:29 +0200

    Feat: add TV

--- src/components/AddTvForm.vue
+++ src/components/AddTvForm.vue
@@ -4,15 +4,20 @@
       <div class="flex justify-center w-[90rem] p-2 rounded-md bg-gray-200 drop-shadow-md">
           <div class="w-full p-5 text-left">
             <h1 class="mb-2 text-5xl font-medium text-center">Add TV</h1>
-            <form action="" method="POST">
+              <div class="bg-primary" v-if="isSuccess">
+                <h1 class="m-2 text-lg text-center text-white">TV Succesfull add</h1>
+              </div>
+              <div class="bg-quaternary" v-if="isFail">
+                <h1 class="m-2 text-lg text-center text-white">TV was not added</h1>
+              </div>
                 <div class="flex flex-row justify-evenly">
                     <div class="flex flex-col mx-1 mb-3 sm:mx-5">
-                        <label for="username" class="text-sm font-medium">TV Name:<br>
-                        <input type="text" id="username" class="p-2 mt-[2px] font-semibold border-2 border-secondary rounded-md w-96" placeholder="TV Name">
+                        <label for="name" class="text-sm font-medium">TV Name:<br>
+                        <input v-model="name" type="text" id="name" class="p-2 mt-[2px] font-semibold border-2 border-secondary rounded-md w-96" placeholder="TV Name">
                         </label>
                     </div>
                     <div class="flex flex-col mx-1 mb-3 sm:mx-5">
-                        <label for="password" class="text-sm font-medium">Group/Team:<br>
+                        <label for="group-team" class="text-sm font-medium">Group/Team:<br>
                             <select id="group-team" class="p-2 mt-[2px] font-semibold border-2 border-secondary rounded-md w-96" placeholder="Group/Team">
                                 <option selected disabled>Choose a Team</option>
                                 <option>No Team</option>
@@ -23,15 +28,15 @@
                         </label>
                     </div>
                     <div class="flex flex-col mx-1 mb-3 sm:mx-5">
-                        <label for="password" class="text-sm font-medium">Ip adress:<br>
-                        <input type="password" id="password" class="p-2 mt-[2px] font-semibold border-2 border-secondary rounded-md w-96" placeholder="Ip adress">
+                        <label for="ipaddress" class="text-sm font-medium">Ip adress:<br>
+                        <input v-model="ipaddress" type="text" id="ipaddress" class="p-2 mt-[2px] font-semibold border-2 border-secondary rounded-md w-96" placeholder="Ip adress">
                         </label>
                     </div>
                 </div>
                 <div class="flex flex-row justify-center mt-2">
-                        <a href="#" class="w-40 p-2 font-medium text-center border-2 rounded-md border-secondary bg-primary">Save</a>
+                        <!-- <a href="#" class="w-40 p-2 font-medium text-center border-2 rounded-md border-secondary bg-primary">Save</a> -->
+                        <button v-on:click="addTv">Save</button>
                 </div>
-            </form>
             </div>
         </div>
     </div>
@@ -39,10 +44,36 @@
 <!-- eslint-enable max-len -->
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'AddTvForm',
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      name: '',
+      ipaddress: '',
+      isSuccess: false,
+      isFail: false,
+    };
+  },
+  methods: {
+    addTv() {
+      axios.post('http://localhost:3000/api/tv/', {
+        name: this.name,
+        ipaddress: this.ipaddress,
+      })
+        .then((response) => {
+          console.log(response.status);
+          this.isSuccess = true;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isFail = true;
+        });
+    },
   },
 };
 </script>
